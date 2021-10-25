0e843f1323deb105af92ef0c2682b5b710e35ff6
Pydgey <97felipe3d@gmail.com>
2021-10-25 20:34:12 -0300

    classes e enum update

---
+++ series/Enum/Genero.cs
@@ -0,0 +1,13 @@
+namespace series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Drama = 4,
+        Romance = 5,
+        Isekai = 6,
+        Shonen = 7,
+    }
+}
